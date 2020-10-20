46c42ddd2dd0a466389879edb179ed9b6d85c8ff
zyrchan <zyrchan@protonmail.com>
2020-10-20 11:49:54 +0000

    a70q: Remove Sepolicies for Vendor
    * For Prebuilt Vendor Build
    
    Signed-off-by: firemax13 <mydadisbrave2@gmail.com>

--- device.mk
+++ device.mk
@@ -80,11 +80,6 @@ PRODUCT_PACKAGES += \
     android.hardware.power@1.0-service \
     power.qcom:64
 
-# QCOM
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/privapp-permissions-qti.xml:system/etc/permissions/privapp-permissions-qti.xml \
-    $(LOCAL_PATH)/configs/qti_whitelist.xml:system/etc/sysconfig/qti_whitelist.xml
-
 # Ramdisk
 PRODUCT_PACKAGES += \
     init.class_late.sh \
