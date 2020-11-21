b910d797e92e3a3d1f57a1589a01413b55f68aa5
CEREBRO <33547482+priyesh2@users.noreply.github.com>
2020-11-21 22:48:41 +0530

    a70q: Audio/Policy: Fix Copy Out Build
    * Fatal Error

--- device.mk
+++ device.mk
@@ -32,7 +32,7 @@ PRODUCT_PACKAGES += \
     audio.a2dp.default
 
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/audio_policy_configuration.xml:$(TARGET_COPY_OUT_PRODUCT)/vendor_overlay/29/etc/audio_policy_configuration.xml
+    $(LOCAL_PATH)/configs/audio_policy_configuration.xml:$(TARGET_COPY_OUT_PRODUCT)/vendor_overlay/(PLATFORM_VNDK_VERSION)/etc/audio_policy_configuration.xml
 
 # Camera
 PRODUCT_PACKAGES += \
