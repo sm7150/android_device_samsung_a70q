763e21cc645a659109020e2967d98d8e2f3efd7a
firemax13 <mydadisbrave2@gmail.com>
2020-11-23 01:24:48 +0000

    a70q: Sensors: Drop & Nuke Old Sensors
    * Compiles into Vendor lmao
    
    Signed-off-by: firemax13 <mydadisbrave2@gmail.com>

--- device.mk
+++ device.mk
@@ -87,10 +87,6 @@ PRODUCT_PACKAGES += \
     libtrafficcontrol-spoofer
 
 # Sensor
-PRODUCT_PACKAGES += \
-    android.hardware.sensors@1.0-impl.a70q \
-    android.hardware.sensors@1.0-service
-
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.sensor.accelerometer.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.sensor.accelerometer.xml \
     frameworks/native/data/etc/android.hardware.sensor.compass.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.sensor.compass.xml \
