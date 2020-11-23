09544e1fe9a18ee5ebaf013765778aa3d7192e46
Grarak <williye97@gmail.com>
2020-11-23 01:30:15 +0000

    a70q: Sensors: Add Custom Impl Sensors
    * Git cherryyyyyyyyyyyyyy
    
    Signed-off-by: firemax13 <mydadisbrave2@gmail.com>

--- device.mk
+++ device.mk
@@ -87,7 +87,11 @@ PRODUCT_PACKAGES += \
     libtrafficcontrol-spoofer
 
 # Sensor
+PRODUCT_PACKAGES += \
+    android.hardware.sensors@1.0-impl.a70q
+
 PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.hardware.sensor.proximity.xml:system/etc/permissions/android.hardware.sensor.proximity.xml \
     frameworks/native/data/etc/android.hardware.sensor.accelerometer.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.sensor.accelerometer.xml \
     frameworks/native/data/etc/android.hardware.sensor.compass.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.sensor.compass.xml \
     frameworks/native/data/etc/android.hardware.sensor.gyroscope.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.sensor.gyroscope.xml \
