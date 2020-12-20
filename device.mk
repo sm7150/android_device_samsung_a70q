04508d31eaf6f6595449382ec095986ef81a8b7f
firemax13 <mydadisbrave2@gmail.com>
2020-12-20 03:18:04 +0000

    a70q: Sensor: Add Missing Proximity Permission
    [BETA]
    
    Signed-off-by: firemax13 <mydadisbrave2@gmail.com>

--- device.mk
+++ device.mk
@@ -102,6 +102,7 @@ PRODUCT_PACKAGES += \
     android.hardware.sensors@1.0-impl.a70q
 
 PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.hardware.sensor.proximity.xml:system/etc/permissions/android.hardware.sensor.proximity.xml \
     frameworks/native/data/etc/android.hardware.sensor.accelerometer.xml:system/etc/permissions/android.hardware.sensor.accelerometer.xml \
     frameworks/native/data/etc/android.hardware.sensor.compass.xml:system/etc/permissions/android.hardware.sensor.compass.xml \
     frameworks/native/data/etc/android.hardware.sensor.gyroscope.xml:system/etc/permissions/android.hardware.sensor.gyroscope.xml \
