839b71f4d332f0e423c1466afc503882346c82ca
jesec <jc@linux.com>
2020-12-30 04:15:38 +0000

    r1q: Declare support for proximity and HiFi sensor
    Signed-off-by: firemax13 <mydadisbrave2@gmail.com>

--- device.mk
+++ device.mk
@@ -99,10 +99,8 @@ PRODUCT_PACKAGES += \
     android.hardware.sensors@1.0-impl.r1q
 
 PRODUCT_COPY_FILES += \
-    frameworks/native/data/etc/android.hardware.sensor.accelerometer.xml:system/etc/permissions/android.hardware.sensor.accelerometer.xml \
-    frameworks/native/data/etc/android.hardware.sensor.compass.xml:system/etc/permissions/android.hardware.sensor.compass.xml \
-    frameworks/native/data/etc/android.hardware.sensor.gyroscope.xml:system/etc/permissions/android.hardware.sensor.gyroscope.xml \
-    frameworks/native/data/etc/android.hardware.sensor.light.xml:system/etc/permissions/android.hardware.sensor.light.xml
+    frameworks/native/data/etc/android.hardware.sensor.proximity.xml:system/etc/permissions/android.hardware.sensor.proximity.xml \
+    frameworks/native/data/etc/android.hardware.sensor.hifi_sensors.xml:system/etc/permissions/android.hardware.sensor.hifi_sensors.xml
 
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
