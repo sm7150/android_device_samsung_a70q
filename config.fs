fb6b2ddd15c14cb28a2a0d8485a0d65c26dfcd71
firemax13 <mydadisbrave2@gmail.com>
2021-01-11 15:35:01 +0800

    a70q: aid_fs_configs: add missing configs of aid fs
    Signed-off-by: firemax13 <mydadisbrave2@gmail.com>

--- config.fs
+++ config.fs
@@ -1,3 +1,27 @@
+[firmware/]
+mode: 0771
+user: AID_SYSTEM
+group: AID_SYSTEM
+caps: 0
+
+[firmware/image/*]
+mode: 0771
+user: AID_SYSTEM
+group: AID_SYSTEM
+caps: 0
+
+[vendor/firmware_mnt/image/*]
+mode: 0771
+user: AID_SYSTEM
+group: AID_SYSTEM
+caps: 0
+
+[bt_firmware/]
+mode: 0771
+user: AID_SYSTEM
+group: AID_SYSTEM
+caps: 0
+
 [efs/]
 mode: 0771
 user: AID_SYSTEM
