cf91d147bb02e66acb57f4865bf836da81ee014d
firemax13 <mydadisbrave2@gmail.com>
2021-01-11 15:50:17 +0800

    a70q: NFC: Properly build NFC & fix read/write ~ beam
    Signed-off-by: firemax13 <mydadisbrave2@gmail.com>

--- device.mk
+++ device.mk
@@ -63,12 +63,9 @@ PRODUCT_PACKAGES += \
     libnfc-nci_jni
 
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/nfc/libnfc-nci.conf:system/etc/libnfc-nci.conf
-
-PRODUCT_COPY_FILES += \
-    frameworks/native/data/etc/android.hardware.nfc.hce.xml:system/etc/permissions/android.hardware.nfc.hce.xml \
-    frameworks/native/data/etc/android.hardware.nfc.hcef.xml:system/etc/permissions/android.hardware.nfc.hcef.xml \
-    frameworks/native/data/etc/android.hardware.nfc.xml:system/etc/permissions/android.hardware.nfc.xml
+    $(LOCAL_PATH)/nfc/libnfc-nci.conf:system/etc/libnfc-nci.conf \
+    $(LOCAL_PATH)/nfc/nfc_key:system/etc/nfc_key \
+    $(LOCAL_PATH)/nfc/nfcee_access.xml:system/etc/nfcee_access.xml
 
 # Power
 PRODUCT_PACKAGES += \
